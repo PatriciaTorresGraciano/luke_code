821b2204c6ab9257b6b35cfe89e7e756441887be
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2020-12-13 17:11:13 -0300

    Subindo arquivos do site Luke Code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import Home from './pages/Home';
+
+import './assets/styles/global.css';
+
+
+function App() {
+  return (
+   <Home/>
+
+  );
+}
+
+export default App;
